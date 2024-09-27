fef768e9648c6bae128c8e159ad4c73401977da1
tantris514 <didi.blond@gmail.com>
2024-09-26 22:56:08 -0400

    Initial Commit

---
+++ TimeTap Watch App/TimeTapApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTapApp.swift
+//  TimeTap Watch App
+//
+//  Created by Tristan Blond on 2024-09-26.
+//
+
+import SwiftUI
+
+@main
+struct TimeTap_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
